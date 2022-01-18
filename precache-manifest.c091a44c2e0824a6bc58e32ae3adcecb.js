fb68ed08d1a4a586affca381dd416045fd750d38
Mikalai Zarazaka <golmtz@gmail.com>
2022-01-18 21:11:16 +0300

    Updates

---
+++ precache-manifest.c091a44c2e0824a6bc58e32ae3adcecb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3412fcaae5a9de4f4e74d27c5b91179d",
+    "url": "/incubator-todo/index.html"
+  },
+  {
+    "revision": "ae45b1aec7cc899dda7c",
+    "url": "/incubator-todo/static/css/main.6b5b00ab.chunk.css"
+  },
+  {
+    "revision": "100e67819595ec974c7d",
+    "url": "/incubator-todo/static/js/2.0a066cd9.chunk.js"
+  },
+  {
+    "revision": "89a1b2dcd30c03705b2bceeb141b76b6",
+    "url": "/incubator-todo/static/js/2.0a066cd9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae45b1aec7cc899dda7c",
+    "url": "/incubator-todo/static/js/main.62b2d1e1.chunk.js"
+  },
+  {
+    "revision": "db5f4c18165ff97e0d6c",
+    "url": "/incubator-todo/static/js/runtime-main.fd0e4130.js"
+  }
+]);
