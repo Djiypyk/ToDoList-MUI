9496ca76cf310189790395b8567cf4d74d62d7f9
Mikalai Zarazaka <golmtz@gmail.com>
2022-02-22 20:50:37 +0300

    add storybook

---
+++ src/Components/AddItemForm.stories.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import {AddItemForm} from "./AddItemForm";
+
+export default {
+    title: 'AddItemForm Component',
+    component: AddItemForm
+}
+
+
+export const AddItemFormBaseExample = () => {
+
+    return <> <AddItemForm addItem={(title: string) => alert(title)}/></>
+}
